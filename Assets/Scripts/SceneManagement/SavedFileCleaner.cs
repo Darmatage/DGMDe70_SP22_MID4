62f2d33aaa0a7d1c7c76a26f508d1b8f90c0d949
Alex Shopov <shopov.alex@gmail.com>
2022-03-03 12:44:12 -0500

    demo build

--- Assets/Scripts/SceneManagement/SavedFileCleaner.cs
+++ Assets/Scripts/SceneManagement/SavedFileCleaner.cs
@@ -5,21 +5,21 @@ using UnityEditor;
    /// This is only in place to delete the save game file eveny tile the game is stopped being played while testing.
     /// </summary>
 
-[InitializeOnLoadAttribute]
+// [InitializeOnLoadAttribute]
 public class SavedFileCleaner : MonoBehaviour
 {
-    // register an event handler when the class is initialized
-    static SavedFileCleaner()
-    {
-        EditorApplication.playModeStateChanged += LogPlayModeState;
-    }
-    private static void LogPlayModeState(PlayModeStateChange state)
-    {
-        if(state == PlayModeStateChange.ExitingPlayMode)
-        {
-            Debug.Log("Delete save file");
-            EventHandler.CallQuitPlayGameEvent();
-        }
-    }
+    // // register an event handler when the class is initialized
+    // static SavedFileCleaner()
+    // {
+    //     EditorApplication.playModeStateChanged += LogPlayModeState;
+    // }
+    // private static void LogPlayModeState(PlayModeStateChange state)
+    // {
+    //     if(state == PlayModeStateChange.ExitingPlayMode)
+    //     {
+    //         Debug.Log("Delete save file");
+    //         EventHandler.CallQuitPlayGameEvent();
+    //     }
+    // }
 
 }
