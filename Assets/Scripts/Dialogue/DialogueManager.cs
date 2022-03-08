a9890213fe93d26850ac825da7927a89fa4c0f86
Stu <stuartmvg@gmail.com>
2022-03-07 22:48:53 -0500

    OpenScreen Method Overload

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -25,6 +25,20 @@ namespace Game.Story
             EventHandler.DialogueActionEvent -= OpenScreen;
         }
 
+        public void OpenScreen(CutSceneDestinationIdentifier cutSceneDestinationIdentifier) 
+        {
+            DialogueVariant variant = DialogueVariant.DV_01;
+            Debug.Log("This is the: " + cutSceneDestinationIdentifier.ToString());
+
+            TMP_Text textBox = dialogueBox.FindComponentInChildrenWithTag<TMP_Text>(Tags.UI_DIALOGUE_SPEECHTEXT_TAG); //<- TMP_Text instead of TextMeshPro
+            textBox.text = gameDialogue.getDialogue(GameScene.Instance.currentScene, GameScene.Instance.currentStage, cutSceneDestinationIdentifier, variant);
+
+            Button closeDialogueButton = dialogueBox.FindComponentInChildrenWithTag<Button>(Tags.UI_BUTTON_DIALOGUE_CLOSE_TAG);
+            closeDialogueButton.onClick.AddListener(() => 
+            {
+                EventHandler.CallCloseAllUIActionEvent();
+            });   
+        }
         public void OpenScreen(CutSceneDestinationIdentifier cutSceneDestinationIdentifier, DialogueVariant variant = DialogueVariant.DV_01) 
         {
             Debug.Log("This is the: " + cutSceneDestinationIdentifier.ToString());
