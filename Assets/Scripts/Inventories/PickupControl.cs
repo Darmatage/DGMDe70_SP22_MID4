e09231c5b0a640e7ffde29a12b86d27106a73a11
Stu <stuartmvg@gmail.com>
2022-03-14 10:26:43 -0400

    Item pickup at distance

--- Assets/Scripts/Inventories/PickupControl.cs
+++ Assets/Scripts/Inventories/PickupControl.cs
@@ -7,11 +7,23 @@ namespace Game.Inventories
     [RequireComponent(typeof(Pickup))]
     public class PickupControl : MonoBehaviour
     {
-        Pickup pickup;
+        [SerializeField] float attrackDistance = 3f;
+        [SerializeField] float moveSpeed = 3f;
+        private Pickup pickup;
+        private GameObject player;
 
         private void Awake()
         {
             pickup = GetComponent<Pickup>();
+            player = GameObject.FindWithTag(Tags.PLAYER_TAG);
+        }
+
+        private void Update() 
+        {
+            if (InRangeOfPlayer())
+            {
+                MoveTo(player.transform.position);
+            }
         }
 
         void OnTriggerEnter2D(Collider2D other)
@@ -20,6 +32,25 @@ namespace Game.Inventories
             {
                 pickup.PickupItem();
             }
+        }
+
+        private bool InRangeOfPlayer()
+        {  
+            float distanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
+            return distanceToPlayer < attrackDistance;
+        }
+
+        private void MoveTo(Vector2 destination)
+        {
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, destination, step);
+        }
+
+        //Called by Unity to visulize the guard area
+        private void OnDrawGizmos() 
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, attrackDistance);
         }
     }
 }
